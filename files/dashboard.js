3e4be88d1afd5a01723a179e90ef3b366c2efa98
Daniel Biedermann <daniel.biedermann@cmore-automotive.com>
2021-07-29 15:11:00 +0200

    update

--- files/dashboard.js
+++ files/dashboard.js
@@ -1,190 +1,43 @@
-// let contextPolar1 = document.getElementById("polarChartLE1").getContext("2d");
-// let contextPolar2 = document.getElementById("polarChartLE2").getContext("2d");
-// let contextPolar3 = document.getElementById("polarChartLE3").getContext("2d");
-
-function createRadarChart(elementName, title) {
-    console.log("Creating Radar Dashboard");
-    let context = document.getElementById(elementName);
-    let chart = echarts.init(context);
-
-    let options = {
-        color: colors,
-        tooltip: {
-            trigger: "item"
-        },
-        legend: {
-            top: "5%",
-            left: "center"
-        },
-        radar: {
-            indicator: [
-                { name: "Einheit 1", min: 0, max: 10 },
-                { name: "Einheit 2", min: 0, max: 10 },
-                { name: "Einheit 3", min: 0, max: 10 },
-                { name: "Einheit 4", min: 0, max: 10 },
-                { name: "Einheit 5", min: 0, max: 10 },
-            ]
-        },
-        series: [
-            {
-                name: "Selbstreguliertes Lernen",
-                type: "radar",
-                data: [
-                    {
-                        name: "Elaboration",
-                        value: getRandomNumbers(5, 0, 10)
-                    },
-                    {
-                        name: "Regulation",
-                        value: getRandomNumbers(5, 0, 10)
-                    },
-                    {
-                        name: "Monitoring",
-                        value: getRandomNumbers(5, 0, 10)
-                    },
-                    {
-                        name: "Planung",
-                        value: getRandomNumbers(5, 0, 10)
-                    }
-                ]
-            }
-
-        ]
+const toolBoxOptions = {
+    show: true,
+    orient: 'vertical',
+    left: 'right',
+    top: 'center',
+    feature: {
+        mark: { show: true },
+        dataView: { show: true, readOnly: false },
+        magicType: { show: true, type: ['line', 'bar'] },
+        restore: { show: true },
+        saveAsImage: { show: true }
     }
+};
 
-    options && chart.setOption(options);
-}
-
-function createBarChart(elementName) {
-    console.log("Creating Bar Charts");
-    let context = document.getElementById(elementName);
-    let chart = echarts.init(context);
-
-    let options = {
-        color: colors,
-        legend: {
-            data: ["Lernzeit", "Lerninteresse", "Lernfreude"]
-        },
-        xAxis: [
-            {
-                type: "category",
-                data: ["Einheit 1", "Einheit 2", "Einheit 3", "Einheit 4", "Einheit 5"]
-            }
-        ],
-        yAxis: [
-            {
-                type: 'value'
-            }
-        ],
-        series: [
-            {
-                name: "Lernzeit",
-                type: "bar",
-                barGap: 0.1,
-                data: getRandomNumbers(5, 0, 10)
-            },
-            {
-                name: "Lerninteresse",
-                type: "bar",
-                barGap: 0.1,
-                data: getRandomNumbers(5, 0, 10)
-            },
-            {
-                name: "Lernfreude",
-                type: "bar",
-                barGap: 0.1,
-                data: getRandomNumbers(5, 0, 10)
-            },
-        ]
-    }
-
-    options && chart.setOption(options);
-}
-
-
-// let radarChartOptions = {
-//     scale: {
-//         min: 0,
-//         max: 10
-//     }
-// };
-
-// new Chart(contextPolar1, {
-//     type: 'radar',
-//     data: {
-//         labels: labels,
-//         datasets: [{
-//             label: "Lernzeit",
-//             data: getRandomNumbers(5, 1, 10),
-//             borderColor: colors[0],
-//         },]
-//     },
-//     options: radarChartOptions
-// });
-
-
-
-// new Chart(contextPolar2, {
-//     type: 'radar',
-//     data: {
-//         labels: labels,
-//         datasets: [{
-//             label: "Lerninteresse",
-//             data: getRandomNumbers(5, 1, 10),
-//             borderColor: colors[2],
-//         },]
-//     },
-//     options: radarChartOptions
-// });
-
-
-// new Chart(contextPolar3, {
-//     type: 'radar',
-//     data: {
-//         labels: labels,
-//         datasets: [{
-//             label: "Lernfreude",
-//             data: getRandomNumbers(5, 1, 10),
-//             borderColor: colors[4],
-//         },]
-//     },
-//     options: radarChartOptions
-// });
-
-
-var contextBarChartReading = document.getElementById("barChartMDC").getContext("2d");
-
-new Chart(contextBarChartReading, {
-    type: 'bar',
-    data: {
-        labels: ["Text 1", "Text 2", "Text 3", "Text 4", "Text 5"],
-        datasets: [
-            {
-                label: "Ihre Zeit",
-                data: getRandomNumbers(5, -10, 10),
-                backgroundColor: colors[2],
-            },]
-    },
-    options: {
-        responsive: false,
-        scales: {
-            y: {
-                min: -10,
-                max: 10,
-                beginAtZero: true
-            }
-        }
-    }
-});
-
-
-function createLEBarChart() {
+function createLEBarChart(highlightIndex) {
 
     var chartDom = document.getElementById('canvas-eng');
     var myChart = echarts.init(chartDom);
     var option;
 
+    let constructs = ['Aufmerksamkeit', 'Anstrengungsbereitschaft', 'Interesse am Lerngegenstand'];
+    let series = [];
+    for (let i = 0; i < constructs.length; i++) {
+        let data = getRandomNumbers(NUM_ASSIGNMENTS, 1, 10);
+
+        data = highlightOnlyOne(data, highlightIndex, colors[i]);
+
+        series.push({
+            name: constructs[i],
+            type: 'bar',
+            barGap: 0.1,
+            emphasis: {
+                focus: 'group'
+            },
+            data
+        });
+    }
+
     option = {
+        color: colors,
         tooltip: {
             trigger: 'axis',
             axisPointer: {
@@ -195,19 +48,7 @@ function createLEBarChart() {
             orient: "vertical",
             data: ['Aufmerksamkeit', 'Anstrengungsbereitschaft', 'Interesse am Lerngegenstand']
         },
-        toolbox: {
-            show: true,
-            orient: 'vertical',
-            left: 'right',
-            top: 'center',
-            feature: {
-                mark: { show: true },
-                dataView: { show: true, readOnly: false },
-                magicType: { show: true, type: ['line', 'bar'] },
-                restore: { show: true },
-                saveAsImage: { show: true }
-            }
-        },
+        toolbox: toolBoxOptions,
         xAxis: [
             {
                 type: 'category',
@@ -220,41 +61,232 @@ function createLEBarChart() {
                 type: 'value'
             }
         ],
-        series: [
-            {
-                name: 'Aufmerksamkeit',
-                type: 'bar',
-                barGap: 0.1,
-                emphasis: {
-                    focus: 'series'
-                },
-                data: getRandomNumbers(5, 0, 10)
-            },
-            {
-                name: 'Anstrengungsbereitschaft',
-                type: 'bar',
-                barGap: 0.1,
-                emphasis: {
-                    focus: 'series'
-                },
-                data: getRandomNumbers(5, 0, 10)
-            },
-            {
-                name: 'Interesse am Lerngegenstand',
-                type: 'bar',
-                barGap: 0.1,
-                emphasis: {
-                    focus: 'series'
-                },
-                data: getRandomNumbers(5, 0, 10)
-            }
-        ]
+        series
     };
 
     option && myChart.setOption(option);
 }
 
 
-createRadarChart("radarSRL");
-// createBarChart("barChartLE");
-createLEBarChart();
+function createSRLBarChart(highlightIndex) {
+    console.log("Creating an SRL Bar Chart");
+
+    var chartDom = document.getElementById('canvas-srl');
+    var myChart = echarts.init(chartDom);
+    var option;
+
+    let constructs = ['Elaboration', 'Regulation', 'Monitoring', 'Planung'];
+    let series = [];
+    for (let i = 0; i < constructs.length; i++) {
+        let data = getRandomNumbers(NUM_ASSIGNMENTS, 1, 10);
+
+        data = highlightOnlyOne(data, highlightIndex, colors[i]);
+
+        series.push({
+            name: constructs[i],
+            type: 'bar',
+            barGap: 0.1,
+            emphasis: {
+                focus: 'category'
+            },
+            data
+        });
+    }
+
+    option = {
+        color: colors,
+        tooltip: {
+            trigger: 'axis',
+            axisPointer: {
+                type: 'shadow'
+            }
+        },
+        legend: {
+            orient: "horizontal",
+            data: constructs
+        },
+        toolbox: toolBoxOptions,
+        xAxis: [
+            {
+                type: 'category',
+                axisTick: { show: false },
+                data: ['Einheit 1', 'Einheit 2', 'Einheit 3', 'Einheit 4', 'Einheit 5']
+            }
+        ],
+        yAxis: [
+            {
+                type: 'value'
+            }
+        ],
+        series
+    };
+
+    option && myChart.setOption(option);
+}
+
+
+function createMDCBarChart(highlightIndex) {
+
+    var chartDom = document.getElementById('canvas-bar-mdc');
+    var myChart = echarts.init(chartDom);
+    var option;
+
+    let min = -10;
+    let max = 10;
+
+    constructs = ['Ihre Zeit']
+    let numbers = getRandomNumbers(5, min, max);
+    let seriesData = [];
+    for (let i = 0; i < 5; i++) {
+        let color = numbers[i] > 0 ? "#44cc44" : '#cc4444';
+        seriesData.push({
+            value: numbers[i],
+            itemStyle: {
+                color
+            },
+            type: 'bar'
+        })
+    }
+
+    console.log(seriesData);
+    option = {
+        toolbox: toolBoxOptions,
+        xAxis: [
+            {
+                type: 'category',
+                data: ['Text 1', 'Text 2', 'Text 3', 'Text 4', 'Text 5']
+            }
+        ],
+        yAxis: [
+            {
+                type: 'value',
+                min,
+                max
+            }
+        ],
+        series: [
+            {
+                data: seriesData,
+                type: "bar"
+            },
+        ]
+    };
+
+    option && myChart.setOption(option);
+}
+
+function createLELineChart() {
+    var chartDom = document.getElementById('canvas-eng');
+    var myChart = echarts.init(chartDom);
+    var option;
+
+    let constructs = ['Aufmerksamkeit', 'Anstrengungsbereitschaft', 'Interesse am Lerngegenstand'];
+    let series = [];
+    for (let i = 0; i < constructs.length; i++) {
+        let data = getRandomNumbers(NUM_ASSIGNMENTS, 1, 10);
+
+        series.push({
+            name: constructs[i],
+            type: 'line',
+            barGap: 0.1,
+            emphasis: {
+                focus: 'group'
+            },
+            data
+        });
+    }
+
+    option = {
+        color: colors,
+        tooltip: {
+            trigger: 'axis',
+        },
+        legend: {
+            orient: "vertical",
+            data: constructs
+        },
+        toolbox: toolBoxOptions,
+        xAxis: [
+            {
+                type: 'category',
+                axisTick: { show: false },
+                data: ['Einheit 1', 'Einheit 2', 'Einheit 3', 'Einheit 4', 'Einheit 5']
+            }
+        ],
+        yAxis: [
+            {
+                type: 'value'
+            }
+        ],
+        series
+    };
+
+    option && myChart.setOption(option);
+}
+
+
+function createSRLLineChart() {
+    var chartDom = document.getElementById('canvas-srl');
+    var myChart = echarts.init(chartDom);
+    var option;
+
+    let constructs = ['Elaboration', 'Regulation', 'Monitoring', 'Planung'];
+    let series = [];
+    for (let i = 0; i < constructs.length; i++) {
+        let data = getRandomNumbers(NUM_ASSIGNMENTS, 1, 5);
+
+        series.push({
+            name: constructs[i],
+            type: 'line',
+            barGap: 0.1,
+            emphasis: {
+                focus: 'group'
+            },
+            data
+        });
+    }
+
+    option = {
+        color: colors,
+        tooltip: {
+            trigger: 'axis',
+        },
+        legend: {
+            orient: "vertical",
+            data: constructs
+        },
+        toolbox: toolBoxOptions,
+        xAxis: [
+            {
+                type: 'category',
+                axisTick: { show: false },
+                data: ['Einheit 1', 'Einheit 2', 'Einheit 3', 'Einheit 4', 'Einheit 5']
+            }
+        ],
+        yAxis: [
+            {
+                type: 'value'
+            }
+        ],
+        series
+    };
+
+    option && myChart.setOption(option);
+}
+
+
+function redrawDashboard(highlightIndex) {
+    createSRLBarChart(highlightIndex);
+    // createRadarChart("radarSRL");
+    // createBarChart("barChartLE");
+    createLEBarChart(highlightIndex);
+    createMDCBarChart(highlightIndex);
+}
+
+function drawTrend(from, to) {
+    createLELineChart();
+    createSRLLineChart();
+}
+
+let NUM_ASSIGNMENTS = 4;
+// redrawDashboard(0);
--- files/shared.js
+++ files/shared.js
@@ -1,7 +1,7 @@
 
 let colors = [
     "#003f5c",
-    "#2f4b7c",
+    "#2f9bac",
     "#665191",
     "#a05195",
     "#d45087",
@@ -11,7 +11,7 @@ let colors = [
 ];
 
 let colorsDiverging = [
-"#6929c4",
+    "#6929c4",
 
 ];
 
@@ -25,5 +25,51 @@ function getRandomNumbers(count, min, max) {
         numbers.push(rndInt);
     }
     console.log(numbers);
+    return numbers;
+}
+
+function getGrayList(list) {
+    let numbers = []
+    for (let i = 0; i < list.length; i++) {
+        if (i == index) {
+            numbers.push(
+                {
+                    value: list[i],
+                    itemStyle: {
+                        color: '#aaaaaa'
+                    }
+                }
+            )
+        } else {
+            numbers.push(list[i])
+        }
+    }
+    return numbers;
+}
+
+function highlightOnlyOne(list, index, color) {
+
+    let numbers = []
+    for (let i = 0; i < list.length; i++) {
+        if (i == index) {
+            numbers.push(
+                {
+                    value: list[i],
+                    itemStyle: {
+                        color: color
+                    }
+                }
+            )
+        } else {
+            numbers.push(
+                {
+                    value: list[i],
+                    itemStyle: {
+                        color: '#aaaaaa44'
+                    }
+                }
+            )
+        }
+    }
     return numbers;
 }
