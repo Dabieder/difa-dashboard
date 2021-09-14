803e5ad619b3ddd450aff22624994214ff42e64e
Daniel Biedermann <dbnakkiel@gmail.com>
2021-09-14 09:53:56 +0200

    mdc donut charts

--- files/dashboard.js
+++ files/dashboard.js
@@ -153,6 +153,54 @@ function createMDCBarChart(highlightIndex) {
     option && myChart.setOption(option);
 }
 
+function createMDCDoughnutChart(canvasId, title) {
+    let chartDom = document.getElementById(canvasId);
+    let myChart = echarts.init(chartDom);
+    let option;
+
+    option = {
+        tooltip: {
+            trigger: 'item'
+        },
+        legend: {
+            top: '5%',
+            left: 'center'
+        },
+        series: [
+            {
+                name: '访问来源',
+                type: 'pie',
+                radius: ['40%', '70%'],
+                avoidLabelOverlap: false,
+                label: {
+                    show: false,
+                    position: 'center'
+                },
+                emphasis: {
+                    label: {
+                        show: true,
+                        fontSize: '40',
+                        fontWeight: 'bold'
+                    }
+                },
+                labelLine: {
+                    show: false
+                },
+                data: [
+                    {value: 1048, name: '搜索引擎'},
+                    {value: 735, name: '直接访问'},
+                    {value: 580, name: '邮件营销'},
+                    {value: 484, name: '联盟广告'},
+                    {value: 300, name: '视频广告'}
+                ]
+            }
+        ]
+    };
+    
+    option && myChart.setOption(option);
+    
+}
+
 function redrawDashboard(highlightIndex) {
     createSRLBarChart(highlightIndex);
     // createRadarChart("radarSRL");
